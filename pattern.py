79c953337fff01fa6a7b8bb48eeffd5d8a14a6d4
ankushrai96 <47821470+ankushrai96@users.noreply.github.com>
2019-03-09 11:59:49 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,7 @@
+a=str(input("tell your name"))
+b=str(input("tell your branch"))
+c=str(input("tell your city\n"))
+
+def pattern():
+	print("\nhii i am \t",a,"\n\t i am studying in ",b,"\n i live in",c)
+pattern()
---
+++ revname.py
@@ -0,0 +1,7 @@
+
+a=str(input("tell your name :"))
+b=str(input("tell your sirname :"))
+
+def revname():
+	print(b,a)
+revname()
